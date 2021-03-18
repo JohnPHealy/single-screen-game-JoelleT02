29b686e49e49c2166cf1ca0e10d2d96bbf4175ad
JoelleT02 <77802831+JoelleT02@users.noreply.github.com>
2021-03-18 15:39:20 +0000

    Changed Player to Ya Boy

--- SingleScreenGame/Assets/Scripts/PlayerMovement.cs
+++ SingleScreenGame/Assets/Scripts/PlayerMovement.cs
@@ -13,14 +13,26 @@ public class PlayerMovement : MonoBehaviour
     private float moveDir;
     private Rigidbody2D myRB;
     private bool canJump;
+    private SpriteRenderer mySprite;
 
     private void Start()
     {
         myRB = GetComponent<Rigidbody2D>();
+        mySprite = GetComponentInChildren<SpriteRenderer>();
     }
 
     private void FixedUpdate()
     {
+        if (moveDir > 0)
+        {
+            mySprite.flipX = true;
+        }
+
+        if (moveDir < 0)
+        {
+            mySprite.flipX = false;
+        }
+        
         var moveAxis = Vector3.right * moveDir;
 
         if (-maxSpeed < myRB.velocity.x && myRB.velocity.x < maxSpeed)
